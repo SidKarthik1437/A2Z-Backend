bec812662a87be22c1a34a781aba5afc9b9107b8
rohankbd <rohankbd.1@gmail.com>
2023-07-20 16:47:26 +0530

    Changes few models and updated API_doc.txt

--- A2Z/settings.py
+++ A2Z/settings.py
@@ -81,27 +81,27 @@ WSGI_APPLICATION = 'A2Z.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'A2Z',
-#         'USER': 'postgres',
-#         'PASSWORD': 'abc@123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'A2Z',
         'USER': 'postgres',
-        'PASSWORD': 'anjan',
+        'PASSWORD': 'abc@123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'anjan',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
--- A2Zbackend/models.py
+++ A2Zbackend/models.py
@@ -3,17 +3,26 @@ from django.db import models
 class Accounts(models.Model):
     account_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    create_date = models.DateField(auto_now_add=True)
+    create_date = models.DateField(auto_now_add=True) 
     address = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=100)
     # alternate_phone = models.CharField(max_length=100)
     account_type = models.CharField(max_length=100)
 
+    def save(self, *args, **kwargs):
+        if not self.account_id:
+            last_account = Accounts.objects.order_by('-account_id').first()
+            if last_account:
+                self.account_id = last_account.account_id + 1
+            else:
+                self.account_id = 1
+        super(Accounts, self).save(*args, **kwargs)
+
 class AccountTypes(models.Model):
     account_type_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    #create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
 
 class Cases(models.Model):
     case_id = models.AutoField(primary_key=True)
@@ -21,7 +30,7 @@ class Cases(models.Model):
     csr_id = models.ForeignKey('SystemUser',on_delete=models.CASCADE)
 
 class Company(models.Model):
-    company_id = models.IntegerField()
+    company_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     email = models.EmailField()
@@ -30,7 +39,7 @@ class Company(models.Model):
     state = models.CharField(max_length=100)
     pincode = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     company_type = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
     auth_token = models.CharField(max_length=100)
@@ -46,7 +55,7 @@ class CompanyFeatures(models.Model):
     company_features_id = models.IntegerField()
     feature_id = models.ForeignKey('Features', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
 
 class CompanyPricing(models.Model):
     id = models.AutoField(primary_key=True)
@@ -108,7 +117,7 @@ class DispatchEntryAssets(models.Model):
     license_plate = models.CharField(max_length=100)
     license_state = models.CharField(max_length=100)
     license_year = models.IntegerField()
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     
 class DispatchEntryStatusRecords():
     id = models.AutoField(primary_key=True)
@@ -119,7 +128,7 @@ class DispatchEntryStatusRecords():
     latitude = models.FloatField()
     longitude = models.FloatField()
     driver_id =  models.IntegerField()
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
 
 class DispatchStatus(models.Model):
     dispatch_status_id = models.AutoField(primary_key=True)
@@ -127,21 +136,30 @@ class DispatchStatus(models.Model):
 
 class DriverLocation(models.Model):
     driverLocation_id = models.AutoField(primary_key=True)
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     company_id = models.ForeignKey('Company', on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
     # client_version_id = models.IntegerField()
     company_id = models.ForeignKey('Company', on_delete=models.CASCADE)
+    
+    def save(self, *args, **kwargs):
+        if not self.driverLocation_id:
+            last_driver_location = DriverLocation.objects.order_by('-driverLocation_id').first()
+            if last_driver_location:
+                self.driverLocation_id = last_driver_location.driverLocation_id + 1
+            else:
+                self.driverLocation_id = 1
+        super(DriverLocation, self).save(*args, **kwargs)
 
 class Features(models.Model):
     feature_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
 
 class Invoices(models.Model):
     invoice_id = models.AutoField(primary_key=True)
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     reference_number = models.CharField(max_length=100)
     ponumber = models.CharField(max_length=100)
     txn_number = models.CharField(max_length=100)
@@ -161,7 +179,7 @@ class Payments(models.Model):
     txn_number = models.CharField(max_length=100)
     payment_type = models.CharField(max_length=100)
     amount = models.FloatField()
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=100)
 
 class RateItem(models.Model):
@@ -183,12 +201,30 @@ class RateItem(models.Model):
     premium_4w = models.IntegerField()
     luxury_4w = models.IntegerField()
     suv_4w = models.IntegerField()
+    
+    def save(self, *args, **kwargs):
+        if not self.rate_item_id:
+            last_driver_location = RateItem.objects.order_by('-rate_item_id').first()
+            if last_driver_location:
+                self.rate_item_id = last_driver_location.rate_item_id + 1
+            else:
+                self.rate_item_id = 1
+        super(RateItem, self).save(*args, **kwargs)
 
 class Reasons(models.Model):
     reason_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     service_type = models.CharField(max_length=100)
+    
+    def save(self, *args, **kwargs):
+        if not self.reason_id:
+            last_driver_location = Reasons.objects.order_by('-reason_id').first()
+            if last_driver_location:
+                self.reason_id = last_driver_location.reason_id + 1
+            else:
+                self.reason_id = 1
+        super(Reasons, self).save(*args, **kwargs)
 
 class ServiceTypes(models.Model):
     service_type_id = models.AutoField(primary_key=True)
@@ -198,7 +234,7 @@ class ServiceTypes(models.Model):
 class SystemUser(models.Model):
     csr_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     role=models.CharField(max_length=50)
     role_id = models.IntegerField()
     status = models.CharField(max_length=100)
@@ -208,7 +244,7 @@ class SystemUserStatusRecords(models.Model):
     dispatch_entry_id = models.ForeignKey('DispatchEntry', on_delete=models.CASCADE)
     previous_csr_id = models.IntegerField()
     new_csr_id=models.IntegerField()
-    create_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
     
 class Vehicles(models.Model):
     vehicle_id = models.AutoField(primary_key=True)
@@ -219,3 +255,12 @@ class Vehicles(models.Model):
     # year = models.IntegerField()
     vehicle_class = models.CharField(max_length=100)
     vehicle_type = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        if not self.vehicle_id:
+            last_vehicle = Vehicles.objects.order_by('-vehicle_id').first()
+            if last_vehicle:
+                self.vehicle_id = last_vehicle.vehicle_id + 1
+            else:
+                self.vehicle_id = 1
+        super(Vehicles, self).save(*args, **kwargs)
